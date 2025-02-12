d47d45135ffc753b2bee857ed07a48f4a86620fa
Jonas Wanke <contact@jonas-wanke.com>
2025-02-12 19:20:17 +0100

    Create Pub workspace, upgrade to Dart ^3.6.0

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   inputs = {
-    # Dart 3.4.2: https://github.com/NixOS/nixpkgs/commit/483914124e9ec4808e97d444b280188fc59ea0b9
-    nixpkgs.url =
-      "github:nixos/nixpkgs?ref=483914124e9ec4808e97d444b280188fc59ea0b9";
-    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
