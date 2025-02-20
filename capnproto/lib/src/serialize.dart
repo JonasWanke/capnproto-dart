5102df719ed21a3512ab7305b1b9c2fb948aa5e3
Jonas Wanke <contact@jonas-wanke.com>
2025-02-20 23:33:17 +0100

    Add tryReadMessage(…)

--- capnproto/lib/src/serialize.dart
+++ capnproto/lib/src/serialize.dart
@@ -1,26 +1,133 @@
 import 'dart:typed_data';
 
+import 'package:meta/meta.dart';
 import 'package:oxidized/oxidized.dart';
 
 import 'constants.dart';
 import 'error.dart';
 import 'message.dart';
+import 'utils.dart';
 
-/// Reads a serialized message (including a segment table) from a flat slice of
-/// bytes, without copying.
+const segmentCountLimit = 512;
+
+/// Reads a serialized message (including a segment table) from [data], without
+/// copying.
 ///
-/// The slice is allowed to extend beyond the end of the message.
+/// [data] is allowed to extend beyond the end of the message.
+///
+/// The segment table format for streams is defined in the Cap'n Proto
+/// [encoding spec](https://capnproto.org/encoding.html).
+@useResult
 CapnpResult<MessageReader> readMessage(
   ByteData data, {
   ReaderOptions options = const ReaderOptions(),
 }) {
-  switch (_readSegmentTable(data, options)) {
+  switch (tryReadMessage(data, options: options)) {
     case Ok(value: null):
-      return const Err(PrematureEndOfFileCapnpError());
-    case Ok(value: final segments?):
-      return Ok(MessageReader(segments, options: options));
+      return const Err(PrematureEndOfInputCapnpError());
+    case Ok(:final value?):
+      return Ok(value);
     case Err(:final error):
       return Err(error);
+  }
+}
+
+/// Like [readMessage], but returns `Ok(null)` instead of an error if the
+/// [data] is empty.
+@useResult
+CapnpResult<MessageReader?> tryReadMessage(
+  ByteData data, {
+  ReaderOptions options = const ReaderOptions(),
+}) {
+  if (data.lengthInBytes == 0) return const Ok(null);
+  if (data.lengthInBytes < 4) return const Err(PrematureEndOfInputCapnpError());
+
+  final int segmentCount;
+  switch (data.getSegmentCount()) {
+    case Ok(:final value):
+      segmentCount = value;
+    case Err(:final error):
+      return Err(error);
+  }
+
+  if (data.lengthInBytes < 4 + segmentCount * 4) {
+    return const Err(PrematureEndOfInputCapnpError());
+  }
+
+  final SegmentLengthsBuilder builder;
+  final int offset;
+  switch (data.offsetBytes(4).getSegmentLengths(segmentCount, options)) {
+    case Ok(:final value):
+      builder = value.builder;
+      offset = 4 + value.offset;
+    case Err(:final error):
+      return Err(error);
+  }
+
+  if (data.lengthInBytes < offset + builder.totalWords) {
+    return const Err(PrematureEndOfInputCapnpError());
+  }
+
+  return Ok(
+    MessageReader(
+      builder.intoSegments(data.offsetBytes(offset)),
+      options: options,
+    ),
+  );
+}
+
+extension on ByteData {
+  CapnpResult<int> getSegmentCount() {
+    final segmentCount = getUint32(0, Endian.little) + 1;
+    if (segmentCount > segmentCountLimit || segmentCount == 0) {
+      return Err(InvalidNumberOfSegmentsCapnpError(segmentCount));
+    }
+    return Ok(segmentCount);
+  }
+
+  CapnpResult<({SegmentLengthsBuilder builder, int offset})> getSegmentLengths(
+    int segmentCount,
+    ReaderOptions options,
+  ) {
+    final builder = SegmentLengthsBuilder();
+    var offset = 0;
+    for (var i = 0; i < segmentCount; i++) {
+      final length = getUint32(offset, Endian.little);
+      builder.addSegment(length);
+      offset += 4;
+    }
+    if (segmentCount.isEven) offset += 4;
+
+    // Don't accept a message which the receiver couldn't possibly traverse
+    // without hitting the traversal limit. Without this check, a malicious
+    // client could transmit a very large segment size to make the receiver
+    // allocate excessive space and possibly crash.
+    if (options.traversalLimitWords case final limit?) {
+      if (builder.totalWords > limit) {
+        return Err(MessageTooLargeCapnpError(builder.totalWords));
+      }
+    }
+
+    return Ok((builder: builder, offset: offset));
+  }
+}
+
+class SegmentLengthsBuilder {
+  SegmentLengthsBuilder();
+
+  final List<({int offsetWords, int lengthWords})> segmentIndices = [];
+
+  var _totalWords = 0;
+  int get totalWords => _totalWords;
+
+  void addSegment(int lengthWords) {
+    segmentIndices.add((offsetWords: totalWords, lengthWords: lengthWords));
+    _totalWords += lengthWords;
+  }
+
+  Segments intoSegments(ByteData data) {
+    assert(data.lengthInBytes == _totalWords * CapnpConstants.bytesPerWord);
+    return Segments(segmentIndices, data);
   }
 }
 
@@ -53,77 +160,6 @@ void _writeSegments(List<ByteData> segments, Sink<List<int>> sink) {
     sink.add(
       segment.buffer.asUint8List(segment.offsetInBytes, segment.lengthInBytes),
     );
-  }
-}
-
-const segmentCountLimit = 512;
-
-/// Reads a segment table from `read` and returns the total number of words
-/// across all segments, as well as the segment offsets.
-///
-/// The segment table format for streams is defined in the Cap'n Proto
-/// [encoding spec](https://capnproto.org/encoding.html)
-CapnpResult<Segments?> _readSegmentTable(
-  ByteData data,
-  ReaderOptions options,
-) {
-  if (data.lengthInBytes == 0) return const Ok(null);
-  if (data.lengthInBytes < 4) return const Err(PrematureEndOfFileCapnpError());
-
-  final segmentCount = data.getUint32(0, Endian.little) + 1;
-  if (segmentCount > segmentCountLimit || segmentCount == 0) {
-    return Err(InvalidNumberOfSegmentsCapnpError(segmentCount));
-  }
-
-  var offset = 4;
-  if (data.lengthInBytes < offset + segmentCount * 4) {
-    return const Err(PrematureEndOfFileCapnpError());
-  }
-
-  final builder = SegmentLengthsBuilder();
-  for (var i = 0; i < segmentCount; i++) {
-    final length = data.getUint32(offset, Endian.little);
-    builder.addSegment(length);
-    offset += 4;
-  }
-
-  // Don't accept a message which the receiver couldn't possibly traverse
-  // without hitting the traversal limit. Without this check, a malicious client
-  // could transmit a very large segment size to make the receiver allocate
-  // excessive space and possibly crash.
-  if (options.traversalLimitWords case final limit?) {
-    if (builder.totalWords > limit) {
-      return Err(MessageTooLargeCapnpError(builder.totalWords));
-    }
-  }
-
-  if (segmentCount.isEven) offset += 4;
-
-  if (data.lengthInBytes < offset + builder.totalWords) {
-    return const Err(PrematureEndOfFileCapnpError());
-  }
-
-  return Ok(
-    builder.intoSegments(data.buffer.asByteData(data.offsetInBytes + offset)),
-  );
-}
-
-class SegmentLengthsBuilder {
-  SegmentLengthsBuilder();
-
-  final List<({int offsetWords, int lengthWords})> segmentIndices = [];
-
-  var _totalWords = 0;
-  int get totalWords => _totalWords;
-
-  void addSegment(int lengthWords) {
-    segmentIndices.add((offsetWords: totalWords, lengthWords: lengthWords));
-    _totalWords += lengthWords;
-  }
-
-  Segments intoSegments(ByteData data) {
-    assert(data.lengthInBytes == _totalWords * CapnpConstants.bytesPerWord);
-    return Segments(segmentIndices, data);
   }
 }
 
